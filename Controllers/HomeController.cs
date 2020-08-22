2d037c6a62c9ce9a97049174e150d607847d36bd
PerezDC <61435556+PerezDC@users.noreply.github.com>
2020-08-21 22:06:30 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using LanguageFeatures.Models;
+using System;
+using System.Linq;
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index() =>
+            View(Product.GetProducts().Select(p => p?.Name));
+    }
+}
